c1c6bee9809974c9cbf056fdf170ec219261ee04
Adam Green <adam.green@adgefficiency.com>
2019-12-06 21:57:34 +0100

    refactor into use cases

--- nasa.py
+++ nasa.py
@@ -1,68 +1,89 @@
-import os
+from ftplib import FTP
+from pathlib import Path
 
 import pandas as pd
 import requests
 
-home = os.environ['HOME']
 
-def nasa_giss():
-    print('NASA Goddard Institute for Space Studies')
-    print('https://data.giss.nasa.gov/gistemp/')
-
-    glbl = 'https://data.giss.nasa.gov/gistemp/tabledata_v4/GLB.Ts+dSST.csv'
-    north = 'https://data.giss.nasa.gov/gistemp/tabledata_v4/NH.Ts+dSST.csv'
-    south = 'https://data.giss.nasa.gov/gistemp/tabledata_v4/SH.Ts+dSST.csv'
-
-    res = requests.get(south)
-    os.makedirs('./data', exist_ok=True)
-    with open('./data/south-temps.csv', 'wb') as fi:
-        fi.write(res.content)
-
-    data = pd.read_csv('./data/south-temps.csv', header=1, index_col=0)
-    data = data.replace("***", 0)
-    data = data.iloc[:-1, :]
-    data = data.astype(float)
+class UseCase:
+    def __init__(self, database, name):
+        self.db = database('climate-data/{}'.format(name))
+        self.raw = self.db.setup('raw')
+        self.clean = self.db.setup('clean')
 
 
-def get_csv(name):
-    print('getting {}'.format(name))
-    with open(raw / name, 'wb') as fp:
-        ftp.retrbinary('RETR '+name, fp.write)
+class NasaTemperature(UseCase):
+    def __init__(self, database):
+        super().__init__(database, 'temperature')
 
-    with open(raw / name, 'r') as fi:
-        for num, line in enumerate(fi):
-            if line[0] != '#':
-                break
+    def execute(self):
+        print('getting NASA temperatures')
+        print('NASA Goddard Institute for Space Studies')
+        print('https://data.giss.nasa.gov/gistemp/')
 
-    home = './'
-    data = pd.read_csv(
-        (raw / name).resolve(),
-        skiprows=num
-    )
-    data.to_csv(clean / name)
+        data = {
+            'global': 'https://data.giss.nasa.gov/gistemp/tabledata_v4/GLB.Ts+dSST.csv',
+            'north': 'https://data.giss.nasa.gov/gistemp/tabledata_v4/NH.Ts+dSST.csv',
+            'south': 'https://data.giss.nasa.gov/gistemp/tabledata_v4/SH.Ts+dSST.csv'
+        }
+
+        for name, url in data.items():
+            res = requests.get(url)
+            raw_file = self.raw / '{}.csv'.format(name)
+            with open(raw_file, 'wb') as fi:
+                fi.write(res.content)
+
+            data = pd.read_csv(raw_file, header=1, index_col=0)
+            data = data.replace("***", 0)
+            data = data.iloc[:-1, :]
+            data = data.astype(float)
+            data.to_csv(self.clean / '{}.csv'.format(name))
+
+
+class Files:
+    """ the Unix filesystem """
+    def __init__(self, home):
+        self.home = Path.home() / home
+        self.home.mkdir(exist_ok=True, parents=True)
+
+    def setup(self, name):
+        sub = self.home / name
+        sub.mkdir(exist_ok=True, parents=True)
+        return sub
+
+
+class NasaCarbonPPM(UseCase):
+    def __init__(self, database):
+        super().__init__(database, 'carbon')
+
+    def execute(self):
+        print('getting NASA carbon')
+        ftp = FTP('aftp.cmdl.noaa.gov')
+        ftp.login()
+        ftp.cwd('products')
+        ftp.cwd('trends')
+        ftp.cwd('co2')
+
+        csvs = [fi for fi in ftp.nlst('.') if '.csv' in fi]
+        for csv in csvs:
+            name = csv
+            print('getting {}'.format(name))
+            with open(self.raw / name, 'wb') as fp:
+                ftp.retrbinary('RETR '+name, fp.write)
+
+            with open(self.raw / name, 'r') as fi:
+                for num, line in enumerate(fi):
+                    if line[0] != '#':
+                        break
+
+            data = pd.read_csv(self.raw / name, skiprows=num)
+            data.to_csv(self.clean / name)
+
 
 if __name__ == '__main__':
+    database = Files
+    use_cases = [NasaTemperature, NasaCarbonPPM]
 
-
-    from pathlib import Path
-    home = Path.home() / 'climate-data'
-    home.mkdir(exist_ok=True, parents=True)
-
-    clean = home /  'clean'
-    clean.mkdir(exist_ok=True, parents=True)
-
-    raw = home / 'raw'
-    raw.mkdir(exist_ok=True, parents=True)
-
-    from ftplib import FTP
-    ftp = FTP('aftp.cmdl.noaa.gov')
-    ftp.login()
-    ftp.cwd('products')
-    ftp.cwd('trends')
-    ftp.cwd('co2')
-    print(ftp.retrlines('LIST'))
-    csvs = [fi for fi in ftp.nlst('.') if '.csv' in fi]
-
-    # name = 'co2_gr_mlo.csv'
-    for csv in csvs:
-        get_csv(csv)
+    for uc in use_cases:
+        uc = uc(database)
+        result = uc.execute()
---
+++ readme.md
@@ -0,0 +1,9 @@
+# climate-data
+
+Making climate crisis data available to data scientists.
+
+## Datasets
+
+CO2 concentration
+
+Global temperatures
